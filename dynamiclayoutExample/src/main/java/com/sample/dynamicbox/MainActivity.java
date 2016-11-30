4c53d327258cf2b6d756c4ccdcc45840401cab44
Shanmuga Boopathy <boopathy.s@zohocorp.com>
2016-11-30 10:40:28 +0530

    GIF image

--- dynamiclayoutExample/src/main/java/com/sample/dynamicbox/MainActivity.java
+++ dynamiclayoutExample/src/main/java/com/sample/dynamicbox/MainActivity.java
@@ -2,6 +2,10 @@ package com.sample.dynamicbox;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,11 +24,18 @@ public class MainActivity extends AppCompatActivity {
         dynamicLayout = (DynamicLayout) findViewById(R.id.dynamic_layout);
         listView = (ListView) findViewById(R.id.listview);
 
-        initLayout();
+        dynamicLayout.setClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                initLayout(true);
+            }
+        });
+
+        initLayout(true);
     }
 
-    private void initLayout() {
-        dynamicLayout.showLoadingLayout(true);
+    private void initLayout(boolean hide) {
+        dynamicLayout.showLoadingLayout(hide);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         for (int i = 0; i<100; i++) {
             adapter.add("String " + i + 1);
@@ -36,5 +47,34 @@ public class MainActivity extends AppCompatActivity {
                 dynamicLayout.hideAll();
             }
         }, 2000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_dynamic, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_reload:
+                initLayout(true);
+                break;
+
+            case R.id.menu_network_error:
+                showNetworkError();
+                break;
+
+            case R.id.menu_refresh:
+                initLayout(false);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showNetworkError() {
+        dynamicLayout.showInternetOffLayout(true);
     }
 }
